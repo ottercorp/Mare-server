0a72ca5bcf442d928516c9964f599175d55b5f10
Tsubasahane <wozaiha@gmail.com>
2025-12-24 17:41:01 +0800

    Update moodles

---
+++ MareSynchronosServer/MareSynchronosShared/Migrations/20251224090323_NewMoodlesStruct.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MareSynchronosServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMoodlesStruct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "status_on_dispell",
+                table: "moodles",
+                newName: "chained_status");
+
+            migrationBuilder.AddColumn<int>(
+                name: "chain_trigger",
+                table: "moodles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "modifier",
+                table: "moodles",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "stack_steps",
+                table: "moodles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "chain_trigger",
+                table: "moodles");
+
+            migrationBuilder.DropColumn(
+                name: "modifier",
+                table: "moodles");
+
+            migrationBuilder.DropColumn(
+                name: "stack_steps",
+                table: "moodles");
+
+            migrationBuilder.RenameColumn(
+                name: "chained_status",
+                table: "moodles",
+                newName: "status_on_dispell");
+        }
+    }
+}
--- MareSynchronosServer/MareSynchronosShared/Models/Moodles.cs
+++ MareSynchronosServer/MareSynchronosShared/Models/Moodles.cs
@@ -8,6 +8,29 @@ public class Moodles
     {
         Positive, Negative, Special
     }
+    
+    [Flags]
+    public enum Modifiers : uint // use uint to allow for futureproof options.
+    {
+        None                = 0,
+        CanDispel           = 1u << 0, // Can be dispelled.
+        StacksIncrease      = 1u << 1, // Stackable moodles, when reapplied, can increase their stack count.
+        StacksRollOver      = 1u << 2, // When a stack reaches its cap, it starts over and counts up again.
+        PersistExpireTime   = 1u << 3, // When reapplied, the expire time remains the same.
+        StacksMoveToChain   = 1u << 4, // When a ChainStatus trigger occurs, the current stacks are is carried over.
+        StacksCarryToChain  = 1u << 5, // When the stacks increase and hit max, remaining stacks carry over.
+        PersistAfterTrigger = 1u << 6, // When a ChainStatus trigger occurs, the original moodle remains.
+        // Ideas: Persist original after chain trigger, ext.. 
+    }
+
+// What must occur for a chained status to trigger.
+// Could be expanded upon to be caused by many things.
+    public enum ChainTriggerEnum
+    {
+        Dispel = 0,
+        HitMaxStacks = 1,
+        TimerExpired = 2,
+    }
 
     [Key]
     [MaxLength(36)]
@@ -16,13 +39,20 @@ public class Moodles
     public string Title { get; set; }
     public string Description { get; set; }
     public StatusType Type { get; set; }
-    public string Applier { get; set; }
-    public bool Dispelable  { get; set; }
-    public int Stacks { get; set; }
-    public Guid StatusOnDispell { get; set; }
+    
     public string CustomFXPath { get; set; }
-    public bool StackOnReapply  { get; set; }
-    public int StacksIncOnReapply  { get; set; }
+    public string Applier { get; set; }
+    public bool Dispelable  { get; set; } //
+    public int Stacks { get; set; }
+    public int StackSteps  { get; set; }
+    public Modifiers Modifier { get; set; }
+    
+    public Guid ChainedStatus { get; set; }
+    public ChainTriggerEnum ChainTrigger { get; set; }
+    
+    public bool StackOnReapply  { get; set; } //
+    public int StacksIncOnReapply  { get; set; } //
+    
     public int Days { get; set; }
     public int Hours { get; set; }
     public int Minutes { get; set; }
